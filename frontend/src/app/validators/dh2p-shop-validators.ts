44848f9d4c8316e06169d6d6e9660ff3d426c47d
anddevdev <heorh.popov@gmail.com>
2023-11-22 11:17:36 +0200

    frontend_fixes

--- frontend/src/app/validators/dh2p-shop-validators.ts
+++ frontend/src/app/validators/dh2p-shop-validators.ts
@@ -6,7 +6,7 @@ export class Dh2pShopValidators {
     static notOnlyWhitespace(control: FormControl) : ValidationErrors {
         
         // check if string only contains whitespace
-        if ((control.value != null) && (control.value.trim().length === 0)) {
+        if ((control.value != null) && ( control.value.trim().length === 0)) {
 
             // invalid, return error object
             return { 'notOnlyWhitespace': true };
