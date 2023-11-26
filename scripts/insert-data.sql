190263d9277d0c9641c2ecd39638c1dd1c542c2b
Akeem Ajibare <88968613+Ha3rk@users.noreply.github.com>
2023-11-26 13:12:00 +0200

    Update insert-data.sql
    Added more data to db-script

--- scripts/insert-data.sql
+++ scripts/insert-data.sql
@@ -1,11 +1,34 @@
 -- Insert data into product_category
-INSERT INTO product_category (id, category_name) VALUES (1, 'Books');
+INSERT INTO product_category (id, category_name) VALUES
+    (1, 'Gym Books'),
+    (2, 'Equipments'),
+    (3, 'Wears'),
+    (4, 'Gears'),
+    (5, 'Shoes');
+
 
 -- Insert data into country
-INSERT INTO country (id, code, name) VALUES (1, 'BR', 'Brazil');
+INSERT INTO country (id, code, name) VALUES
+    (1, 'BR', 'Brazil'),
+    (2, 'US', 'Lithuania'),
+    (3, 'NG', 'Nigeria'),
+    (4, 'IR', 'Ireland'),
+    (5, 'SWE', 'Sweden');
 
 -- Insert data into state
-INSERT INTO state (id, name, country_id) VALUES (1, 'Acre', 1);
+INSERT INTO state (id, name, country_id) VALUES
+    (1, 'Acre', 1),
+    (2, 'Kaunas', 2),
+    (3, 'Lagos', 3),
+    (4, 'Dublin', 4),
+    (5, 'Stockholm', 5);
 
 -- Insert data into product
-INSERT INTO product (sku, name, description, image_url, active, units_in_stock, unit_price, category_id, date_created) VALUES ('BOOK-TECH-1000', 'JavaScript - The Fun Parts', 'Learn JavaScript', 'changelog/images/placeholder.png', true, 100, 19.99, 1, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock, unit_price, category_id, date_created) VALUES
+    ('BOOK-GYM-1000', 'Just do it! - The Fun Parts', 'Learn JavaScript', './images/book.png', true, 100, 19.99, 1, NOW()),
+    ('ELEC-Bicycle-2000', 'Smart XYZ', 'High-end max leg-day', './images/bike.png', true, 50, 699.99, 2, NOW()),
+    ('CLOTH-TSHIRT-3000', 'liverpool T-Shirt', 'LFC Jersey', './images/jersey.png', true, 200, 12.99, 3, NOW()),
+    ('Helmet', 'dirt-bike helmet T-Shirt', 'Helmet lightweight', './images/helmet.png', true, 200, 12.99, 3, NOW()),
+    ('Adidas', 'Adidas 360', 'Adidas Macro X5', './images/shoes.png', true, 200, 12.99, 3, NOW());
+
+;
