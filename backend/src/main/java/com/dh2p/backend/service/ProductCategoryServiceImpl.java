8f6273d4048c826b4e1b66812e656cc1dfd1fa51
NorthDragonDuck <113013199+NorthDragonDuck@users.noreply.github.com>
2023-12-03 14:08:46 +0200

    Frontend colors change

--- backend/src/main/java/com/dh2p/backend/service/ProductCategoryServiceImpl.java
+++ backend/src/main/java/com/dh2p/backend/service/ProductCategoryServiceImpl.java
@@ -1,7 +1,4 @@
 package com.dh2p.backend.service;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.dh2p.backend.model.ProductCategory;
--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
-import { timeoutWith } from 'rxjs/operators';
 import { CartItem } from 'src/app/common/cart-item';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -14,13 +13,13 @@ import { CartService } from 'src/app/services/cart.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
-  currentCategorySlug: string = '';  // Use slug instead of ID
-  previousCategorySlug: string = ''; // Track the previous slug for optimization
+  currentCategorySlug: string = '';
+  previousCategorySlug: string = '';
   searchMode: boolean = false;
 
-  // New properties for pagination
+  // Properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 5;
+  thePageSize: number = 10;
   theTotalElements: number = 0;
   theTotalPages: number = 0;
 
@@ -74,24 +73,16 @@ export class ProductListComponent implements OnInit {
     }
   }
 
-  updatePageSize(pageSize: number) {
-    this.thePageSize = pageSize;
-    this.thePageNumber = 1;
-    this.listProducts();
-  }
-
   processResult() {
     return data => {
       this.products = data.content;
-      this.thePageNumber = data.number + 1; // Spring Data REST pages are zero-based
+      this.thePageNumber = data.number + 1;
       this.thePageSize = data.size;
       this.theTotalElements = data.totalElements;
-      // If you want to use totalPages, make sure it's included in the service's interface
-      this.theTotalPages = data.totalPages;
+      this.theTotalPages = data.totalPages; // If using totalPages, ensure it's included in the service's response
     };
   }
   
-
   addToCart(theProduct: Product) {
     let theCartItem = new CartItem(theProduct.sku, theProduct.name, theProduct.imageUrl, theProduct.unitPrice);
     this.cartService.addToCart(theCartItem);
